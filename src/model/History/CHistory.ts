79baebe6924a58b990e7af8cfbcebb6f2b81a1eb
kazantsev-roman <dj.ka3anova@gmail.com>
2022-12-17 02:31:24 +0300

    реализовал историю

---
+++ src/model/History/CHistory.ts
@@ -0,0 +1,72 @@
+import IHistory from "./IHistory"
+import ICommand from "../Commands/ICommand"
+
+class CHistory implements IHistory
+{
+	public CanRedo(): boolean
+	{
+		return this.redo.length > 0
+	}
+
+	public CanUndo(): boolean
+	{
+		return this.undo.length > 0
+	}
+
+	public Redo(): void
+	{
+		if(this.CanRedo())
+		{
+			const command = this.redo.pop()
+			if(command !== undefined)
+			{
+				this.undo.push(command)
+				command.Execute()
+			}
+		}
+		else
+		{
+			throw "CAN'T REDO"
+		}
+	}
+
+	public Undo(): void
+	{
+		if(this.CanUndo())
+		{
+			const command = this.undo.pop()
+			if(command !== undefined)
+			{
+				this.redo.push(command)
+				command.UnExecute()
+			}
+		}
+		else
+		{
+			throw "CAN'T UNDO"
+		}
+	}
+
+	public ExecuteCommand(command: ICommand): void
+	{
+		command.Execute()
+		this.AddExecutedCommand(command)
+	}
+
+	private AddExecutedCommand(command: ICommand): void
+	{
+		this.redo = []
+		if(this.undo.length >= this.MAX_DEPTH_OF_HISTORY)
+		{
+			this.undo.shift()
+		}
+
+		this.undo.push(command)
+	}
+
+	private readonly MAX_DEPTH_OF_HISTORY: number = 20
+	private undo: Array<ICommand> = []
+	private redo: Array<ICommand> = []
+}
+
+export default CHistory
---
+++ src/model/History/IHistory.ts
@@ -0,0 +1,12 @@
+import ICommand from "../Commands/ICommand"
+
+interface IHistory
+{
+	Undo(): void
+	Redo(): void
+	CanUndo(): boolean
+	CanRedo(): boolean
+	ExecuteCommand(command: ICommand): void
+}
+
+export default IHistory
