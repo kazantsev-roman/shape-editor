2494a49c4874556097a53081952a4d229d60b835
kazantsev-roman <dj.ka3anova@gmail.com>
2023-01-16 04:27:58 +0300

    убрал ненужную переменную и if

--- src/view/hooks/useItemResize.tsx
+++ src/view/hooks/useItemResize.tsx
@@ -15,21 +15,16 @@ function useItemResize<T extends Element>(
 	const onDragStart = () => {}
 
 	const onDrag = (delta: { x: number, y: number }) => {
-		let currentSize = {width: frame.width + delta.x, height: frame.height + delta.y}
-
-		if(ref.current?.getBoundingClientRect())
-		{
-			delta.x = frame.width + delta.x < 1
-				? 1
-				: frame.leftTopPoint.x + frame.width + delta.x > Settings.canvasWidth
-					? Settings.canvasWidth - frame.leftTopPoint.x
-					: currentSize.width
-			delta.y = frame.height + delta.y < 1
-				? 1
-				: frame.leftTopPoint.y + frame.height + delta.y > Settings.canvasHeight
-					? Settings.canvasHeight - frame.leftTopPoint.y
-					: currentSize.height
-		}
+		delta.x = frame.width + delta.x < 1
+			? 1
+			: frame.leftTopPoint.x + frame.width + delta.x > Settings.canvasWidth
+				? Settings.canvasWidth - frame.leftTopPoint.x
+				: frame.width + delta.x
+		delta.y = frame.height + delta.y < 1
+			? 1
+			: frame.leftTopPoint.y + frame.height + delta.y > Settings.canvasHeight
+				? Settings.canvasHeight - frame.leftTopPoint.y
+				: frame.height + delta.y
 
 		setSize({width: delta.x, height: delta.y})
 	}
